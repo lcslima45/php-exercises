8dd83714d09a7b48816bb190a6f4bde8974b3947
lcslima45 <lcslima45@github.com>
2018-06-22 12:25:20 -0300

    treinando orientação ao objeto

---
+++ objects.php
@@ -0,0 +1,15 @@
+<?php
+	class Pessoa{
+		var $nome;
+		function setNome($nome){
+			$this->nome = $nome;	
+		}
+		function getNome(){
+			return $this->nome;		
+		}	
+	}
+
+$pessoa = new Pessoa();
+$pessoa->setNome('Joao');
+echo $pessoa->getNome();
+?>
